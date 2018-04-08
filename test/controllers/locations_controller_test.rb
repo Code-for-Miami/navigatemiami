b30554b6ca660f72a8e903a193d12ea1879ead61
Chris Scott <cyberstrike@gmail.com>
2018-04-08 19:00:42 -0400

    passing all required tests

--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -15,12 +15,27 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create location" do
+  test "create location should redirect to new location" do
     assert_difference('Location.count') do
       post locations_url, params: { location: { address1: @location.address1, address2: @location.address2, city: @location.city, email: @location.email, name: @location.name, phone: @location.phone, state: @location.state, url: @location.url, zip: @location.zip } }
     end
 
     assert_redirected_to location_url(Location.last)
+  end
+
+  test "create location should flash success" do
+    assert_difference('Location.count') do
+      post locations_url, params: { location: { address1: @location.address1, address2: @location.address2, city: @location.city, email: @location.email, name: @location.name, phone: @location.phone, state: @location.state, url: @location.url, zip: @location.zip } }
+    end
+
+    assert_equal 'Location was successfully created.', flash[:notice]
+  end
+
+  test "create location failure should show error" do
+    post locations_url, params: { location: { address1: '', address2: @location.address2, city: @location.city, email: @location.email, name: @location.name, phone: @location.phone, state: @location.state, url: @location.url, zip: @location.zip } }
+
+    #       assert_select 'h1', 'Articles'
+    assert_select '#error_explanation>ul', "Address1 can't be blank"
   end
 
   test "should show location" do
@@ -33,9 +48,32 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update location" do
-    patch location_url(@location), params: { location: { address1: @location.address1, address2: @location.address2, city: @location.city, email: @location.email, name: @location.name, phone: @location.phone, state: @location.state, url: @location.url, zip: @location.zip } }
+  test "successful location update should redirect to new location" do
+    patch location_url(@location), params: {
+        location: {
+          address1: @location.address1, address2: @location.address2,
+          city: @location.city, email: @location.email,
+          name: @location.name, phone: @location.phone,
+          state: @location.state, url: @location.url,
+          zip: @location.zip
+        }
+    }
+
     assert_redirected_to location_url(@location)
+  end
+
+  test "successful location update should show success message" do
+    patch location_url(@location), params: {
+        location: {
+            address1: @location.address1, address2: @location.address2,
+            city: @location.city, email: @location.email,
+            name: @location.name, phone: @location.phone,
+            state: @location.state, url: @location.url,
+            zip: @location.zip
+        }
+    }
+
+    assert_equal 'Location was successfully updated.', flash[:notice]
   end
 
   test "should destroy location" do
--- test/system/locations_test.rb
+++ test/system/locations_test.rb
@@ -1,9 +1,10 @@
 require "application_system_test_case"
 
 class LocationsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit locations_url
-  #
-  #   assert_selector "h1", text: "Location"
-  # end
+  test "visiting the index" do
+    visit locations_url
+    # $stderr.write 'Press enter to continue'
+    # $stdin.gets
+    assert_selector "h1", text: "Location"
+  end
 end
