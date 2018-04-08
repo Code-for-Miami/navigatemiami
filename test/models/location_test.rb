f90793a396b7a41c5ef3904290385712f0e6c73f
Chris Scott <cyberstrike@gmail.com>
2018-04-08 17:27:19 -0400

    basics tests wip

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -4,4 +4,33 @@ class LocationTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test 'should not save location without name' do
+    location = Location.new
+    location.save
+
+    assert_equal("can't be blank", location.errors[:name][0])
+  end
+
+  test 'should not save location without address' do
+    location = Location.new
+    location.save
+
+    assert_equal("can't be blank", location.errors[:address1][0])
+  end
+
+  test 'should not save location without city' do
+    location = Location.new
+    location.save
+
+    assert_equal("can't be blank", location.errors[:city][0])
+  end
+
+  test 'should not save location without zip' do
+    location = Location.new
+    location.save
+
+    assert_equal("can't be blank", location.errors[:zip][0])
+  end
+
 end
