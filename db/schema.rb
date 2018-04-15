efcaa619c3cb0a21efeb4b694b1ed30fb280167d
Chris Scott <cyberstrike@gmail.com>
2018-04-15 16:52:47 -0400

    upgraded rails to 5.2 (#11)
    * upgraded rails to 5.2
    
    * made test automigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407205808) do
+ActiveRecord::Schema.define(version: 2018_04_07_205808) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
