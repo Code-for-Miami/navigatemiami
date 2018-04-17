2b894fe562d73563ed9e054ceb9e5893396c8335
Chris Scott <cyberstrike@gmail.com>
2018-04-17 09:02:29 -0400

    added missing stylesheet and set root route

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,23 +1,18 @@
 /* eslint no-console: 0 */
 
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
 import Vue from 'vue/dist/vue.esm'
 import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 import App from '../app.vue'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'));
   const app = new Vue({
-    el,
+    el: '#app',
     render: h => h(App)
   });
 });
